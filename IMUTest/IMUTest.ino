d48b5a229ac4be1f3d84adf978c8b0a42a7db406
eilims <dbenusovich@gmail.com>
2017-11-05 10:24:32 -0800

    Correcting bit precision

--- IMUTest/IMUTest.ino
+++ IMUTest/IMUTest.ino
@@ -14,7 +14,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- uint8_t array[3];
+ uint16_t array[3];
  thingy.getXYZ(array);
  Serial.print("X: ");
  Serial.print(array[0], DEC);
