cc688846ca41bb3e4e7ed29e26f8797dc8e8bb00
eilims <dbenusovich@gmail.com>
2017-11-11 14:14:15 -0800

    Added Comments. Added XBee Ex.

--- IMU/IMUTest/IMUTest.ino
+++ IMU/IMUTest/IMUTest.ino
@@ -16,13 +16,21 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   delay(1000);
+ /* 	The number passed in is the ID number for the sensor. It 	must be unique.
+	The IMU will attempt to calibrate itself by turning and 	stopping repeatedly. Please contact the creator of the 	library if the IMU does not calibrates after several cycles
+*/
   thingy.initialize(0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+ //Declare a 16 bit int array of size three
+ // 16 bits to fully represent the numbers
+ // 3 indices for X Y and Z
  int16_t array[3];
+ //Pass in the array for it to get populated
  thingy.getXYZ(array);
+ //Read from the array! Easy day
  Serial.print("X: ");
  Serial.print(array[0], DEC);
  Serial.print("     Y: ");
--- XBee/XBeeLibReceive/XBeeLibReceive.ino
+++ XBee/XBeeLibReceive/XBeeLibReceive.ino
@@ -10,6 +10,7 @@
 #include <MotorControl.h>
 #include <PinDeclarations.h>
 
+//Setting up xbee as end device
 H2RoverXbee xbee(0);
 
 void setup() {
@@ -19,8 +20,18 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //Make the maximum packet size available as the array
+  //Not all of the array may be used in every message
+  //This issue will be fixed shortly
   uint8_t payload[H2RoverXbee::MAXIMUM_PACKET_SIZE];
+  
+  //Populate array
+  /* The status is important to check. A proper payload must be 	acknowledged and read
+  */
   int status = xbee.getPacket(payload);
+  
+  //Check if the packet status is good then print the results
+  //Or process them as ones could requires.
   if (status == H2RoverXbee::RECIEVED_PACKET_ACK){
     for(int i = 0; i < H2RoverXbee::MAXIMUM_PACKET_SIZE; i++){
       Serial.print(" Index: ");
