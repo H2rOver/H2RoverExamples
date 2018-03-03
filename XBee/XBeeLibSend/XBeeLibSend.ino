7efecf5e5a0a3b9c46629d00a0fbf9224b38442e
jonmidolo <jonathan.midolo@gmail.com>
2018-03-03 15:54:04 -0800

    Added clarification for sending from end->coordinator, coordinator->end

--- XBee/XBeeLibSend/XBeeLibSend.ino
+++ XBee/XBeeLibSend/XBeeLibSend.ino
@@ -1,7 +1,7 @@
 /*	Created By: Daniel Benusovich
 	Created On: 8 November 2017
-	Last Edited By: Daniel Benusovich
-	Last Edited On: 8 November 2017
+	Last Edited By: Daniel Benusovich and Jonathan Midolo
+	Last Edited On: 3 March 2018
 */
 
 #include <H2RoverXbee.h>
@@ -9,7 +9,14 @@
 #include <MotorControl.h>
 #include <PinDeclarations.h>
 
-H2RoverXbee xbee(1);
+// If you want to transmit from END DEVICE to COORDINATOR
+//     comment out all xbee1 references and uncomment all xbee references
+// If you want to transmit from COORDINATOR to END DEVICE
+//     comment out all xbee references and uncomment all xbee1 references
+
+// For EC07(END) to transmit to EC02(COORD) use ID=1
+//H2RoverXbee xbee(1);
+// For EC02(COORD) to transmit to EC07(END) use ID=0
 H2RoverXbee xbee1(0);
 
 void setup() {
@@ -19,11 +26,12 @@ void setup() {
 }
 
 void loop() {
-xbee.setMaximumPacketSize(10);
+//xbee.setMaximumPacketSize(10);
+xbee1.setMaximumPacketSize(10);
   // put your main code here, to run repeatedly:
 //First value holds the length of the payload DO NOT INCLUDE THE  // LENGTH OF THE PAYLOAD. Notice ten characters <= 10
   uint8_t payload[] = {6, 'A' , 'B', 'C', '1', '2', '3'};
-  xbee.sendPacket(payload);
+  //xbee.sendPacket(payload);
   xbee1.sendPacket(payload);
   delay(1000);
 }
